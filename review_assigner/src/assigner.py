98871efe5be24524e0bb710b56da094423fd8814
Michael Green <mcg520@nyu.edu>
2022-02-06 13:19:24 -0500

    adding CSV output format

--- review_assigner/src/assigner.py
+++ review_assigner/src/assigner.py
@@ -10,19 +10,22 @@ class Assigner:
             groups = f.read().splitlines()
         self.groups = groups
     
-    def assign_reviews(self, reviews_per_group=3):
+    def assign_reviews(self, reviews_per_group=3, output_loc=None):
         groups = self.groups
         random.shuffle(groups)
-        csv_format = []
+        csv_format = [["Name"]]
+        csv_format[0] += [f"Review {x}" for x in range(1,4)]
         for i, group in enumerate(groups):
             reviews = [groups[(i+x) % len(groups)] for x in range(1, reviews_per_group+1)]
             print(f"{group}: {reviews}")
             row = [group] + reviews
             csv_format.append(row)
+        if output_loc is not None:
+            with open(output_loc, "w", newline='') as f:
+                writer = csv.writer(f)
+                for row in csv_format:
+                    writer.writerow(row)
         
-
-
-
 if __name__ == "__main__": 
     assigner = Assigner("input/groups.txt")
-    assigner.assign_reviews()
+    assigner.assign_reviews(output_loc="output.csv")
--- run_assigner.py
+++ run_assigner.py
@@ -1,4 +1,4 @@
 from review_assigner.src.assigner import Assigner
 
 assigner = Assigner("input/groups.txt")
-assigner.assign_reviews()
+assigner.assign_reviews(output_loc="output.csv")
